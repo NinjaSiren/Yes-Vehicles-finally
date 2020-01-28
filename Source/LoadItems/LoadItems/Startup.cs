5b5afb984e1869bbe2d45aa8f2ea89cd01650206
Taranchuk <nfhfyxer@gmail.com>
2020-01-28 16:50:01 +0200

    Feature load things into the vehicle

---
+++ Source/LoadItems/LoadItems/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System.IO;
+using Harmony;
+using UnityEngine;
+using Verse;
+
+namespace LoadItems
+{
+    public class Startup : Mod
+    {
+        public Startup(ModContentPack content) : base(content)
+        {
+            Startup.HarmonyInstance = HarmonyInstance.Create("YesVehicle.LoadItems");
+        }
+
+        public static HarmonyInstance HarmonyInstance;
+    }
+}
